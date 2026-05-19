e4d74509d42a4483f4ffe970dbb5fda5cc92ab20
nezar-kanghi <nezar.kanghi@educa.jcyl.es>
2026-05-19 12:10:59 +0200

    Update Ejercicio_1.java

--- src/main/java/org/example/Ejercicio_1.java
+++ src/main/java/org/example/Ejercicio_1.java
@@ -10,11 +10,15 @@ public class Ejercicio_1 extends Application {
     @Override
     public void start(Stage stage) {
 
+        // Creamos un botón con el texto que queremos
         Button btn = new Button("Haz clic aquí");
 
         VBox layout = new VBox(btn);
 
+        // Scene es el contenido de la ventana, 250 de ancho y 100 de alto
         Scene scene = new Scene(layout, 250, 100);
+
+        // Configuramos la ventana
         stage.setTitle("Ejercicio 1");
         stage.setScene(scene);
         stage.show();
